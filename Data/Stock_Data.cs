c80d81eb3659be8ba9ba72e18c19681a0b63e497
unknown <jamesalbu2309@gmail.com>
2020-01-22 00:19:51 -0500

    Added test detail page - WIP

--- Data/Stock_Data.cs
+++ Data/Stock_Data.cs
@@ -9,6 +9,7 @@ namespace Financial_Portfolio.Data
 {
     public class Stock_Data
     {
+        //public string symbol { get; set; }
         public DateTime Timestamp { get; set; }
         public decimal Open { get; set; }
         public decimal High { get; set; }
@@ -17,16 +18,20 @@ namespace Financial_Portfolio.Data
         public decimal Close { get; set; }
         public decimal Volume { get; set; }
 
+        //private string apiKey = "E6ZUIRZSKLBENGRD"; // retrieve your api key from https://www.alphavantage.co/support/#api-key
+
+
+        //TODO - This function probably needs to go somewhere else... maybe by itself
+        // Needs to be its own class to work i think... - JMA 1-22-2020
         public List<Stock_Data> Get_Test_Stocks()
         {
-            // List<Stock_Data> test_stock = new List<Stock_Data>();
-            var symbol = "AAPL";
-            var apiKey = "E6ZUIRZSKLBENGRD"; // retrieve your api key from https://www.alphavantage.co/support/#api-key
-            var test_stock = $"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={apiKey}&datatype=csv"
+            List<Stock_Data> test_stock = new List<Stock_Data>();
+            string symbol = "AAPL";
+            string apiKey = "E6ZUIRZSKLBENGRD"; // retrieve your api key from https://www.alphavantage.co/support/#api-key
+            test_stock = $"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={apiKey}&datatype=csv"
                             .GetStringFromUrl().FromCsv<List<Stock_Data>>();
             
             return test_stock;
         }
     }
-
 }
